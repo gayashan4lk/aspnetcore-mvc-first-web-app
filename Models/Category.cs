67e453f0a23134d86b40f5fe5d9563a202e0a266
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-25 07:55:10 +0530

    add properties to Category class

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,6 +1,12 @@
-﻿namespace BethanysPieShop.Models
+﻿using System.Collections.Generic;
+
+namespace BethanysPieShop.Models
 {
     public class Category
     {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public List<Pie> Pies { get; set; }
     }
 }
